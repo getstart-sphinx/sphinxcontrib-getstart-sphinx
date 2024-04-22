73672c4942e675dffcea0477b11c5f4ae552ee30
Akihiro Takizawa <akihiro.takizawa@gmail.com>
2024-04-22 17:54:23 +0900

    add column title as text node.

--- sphinxcontrib/getstart_sphinx/column.py
+++ sphinxcontrib/getstart_sphinx/column.py
@@ -40,6 +40,8 @@ class ColumnDirective(Admonition):
 
 
 def visit_column(self, node):
+    if node['title']:
+        node.insert(0, nodes.title(node['title'], node['title']))
     self.visit_admonition(node)
 
 
